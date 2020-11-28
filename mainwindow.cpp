6b064a2014e42fd4fefc4a6b1dd6bbc9696641a6
Дмитрий Арещенков <ore111ek@mail.ru>
2020-11-28 11:27:05 +0300

    Queue + Graph

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,49 +1,291 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QPainter>
-#include <QColor>
-
 #include <QTime>
+
+Process::Process(QString aname, int ax, int ay, int alength, int aheight, QColor acolor){
+    x = ax;
+    y = ay;
+    length = startlength = alength;
+    height = aheight;
+    color = acolor;
+    name = aname;
+}
+
+Process::~Process(){
+}
+
+void Process::draw(/*QColor col*/){
+    painter->fillRect(x, y, length, height, QBrush(color));
+    painter->setPen(/*col*/Qt::black);
+    painter->drawRect(x, y, length, height);
+    painter->drawText(QRect(x, y, length, height),name,(QTextOption)Qt::AlignCenter);
+}
+/*
+void Process::show(){
+    draw();
+}
+
+void Process::hide(){
+    QColor col = color;
+    color = QColor(255,255,255,0);
+    draw(color);
+    color = col;
+}*/
+
+void Process::move(int dx, int dy){
+    //hide();
+    x += dx;
+    y += dy;
+    //show();
+    draw();
+}
+
+int Process::getX(){ return x; }
+int Process::getY(){ return y; }
+int Process::getLength(){ return length; }
+int Process::getStartLength(){ return startlength; }
+int Process::getHeight(){ return height; }
+QString Process::getName(){ return name; }
+
+void Process::setX(int ax){ x = ax; }
+void Process::setY(int ay){ y = ay; }
+void Process::setLength(int len){ length = len; }
+void Process::setPainter(QPainter *paint){ painter = paint; }
+
+
+int Area::getOriginX(){ return originX; }
+int Area::getOriginY(){ return originY; }
+int Area::getLengthX(){ return lengthX; }
+int Area::getHeightY(){ return heightY; }
+void Area::setPainter(QPainter *paint){ painter = paint; }
+
+
+Queue::Queue(int aoriginX, int aoriginY,
+             int alengthX, int aheightY, int astepY){
+    originX = aoriginX; originY = aoriginY;
+    lengthX = alengthX; heightY = aheightY;
+    stepY = astepY;
+}
+
+Queue::~Queue(){}
+
+void Queue::drawLabel(){
+    painter->fillRect(originX, originY - heightY - 40, lengthX, 40, QBrush(QColor(255,255,255)));//Закрасить белым над областью, на месте лейбла
+    painter->drawText(QRect(originX, originY - heightY - 40, lengthX, 40),"Очередь",(QTextOption)Qt::AlignCenter);
+    painter->fillRect(originX, originY, lengthX, 40, QBrush(QColor(255,255,255)));//Закрасить белым над областью, на месте лейбла
+    painter->drawRect(originX, originY - heightY, lengthX, heightY); //Рамка области
+}
+void Queue::draw(){
+
+    int y = originY - 35;
+    int heightOfAll = 0;
+    for(auto &process : processes){
+        heightOfAll += stepY;
+    }
+    int space = heightOfAll - heightY;
+    int margin = slider->value()*space/100;
+    for(auto &process : processes){
+
+        if(y > (originY - heightY - margin - process.getHeight()) && y < originY - margin/* - process.getHeight()*/){
+            process.setPainter(painter);
+            //process.setX(originX+10);
+            process.setX(originX + (lengthX - process.getLength())/2);
+            process.setY(y + margin);
+            process.draw();
+
+        }
+         y -= stepY;
+    }
+    drawLabel();
+
+}
+void Queue::sort(){
+    if(sorting->currentText() == "Краткосрочное планирование"){
+
+    }
+
+}
+void Queue::addProcess(Process proc){
+    processes.push_back(proc);
+    sort();
+}
+Process Queue::findProcess(){
+}
+
+int Queue::getStepY(){ return stepY; }
+
+void Queue::setStepY(int step){ stepY = step; }
+void Queue::setSlider(QSlider *sl){ slider = sl; }
+void Queue::setSorting(QComboBox *sort){ sorting = sort; }
+
+
+ExecutionBar::ExecutionBar(int aoriginX, int aoriginY,
+             int alengthX, int aheightY){
+    originX = aoriginX; originY = aoriginY;
+    lengthX = alengthX; heightY = aheightY;
+}
+ExecutionBar::~ExecutionBar(){}
+void ExecutionBar::drawLabel(){
+    painter->drawText(QRect(originX, originY - heightY - 30, lengthX, 30),"Выполняемые процессы",(QTextOption)Qt::AlignCenter);
+    painter->drawRect(originX, originY - heightY, lengthX, heightY);
+}
+void ExecutionBar::draw(){
+
+}
+void ExecutionBar::requestForNext(){
+
+}
+void ExecutionBar::clear(){
+
+}
+
+Graph::Graph(Queue *q, int aoriginX, int aoriginY,
+             int alengthX, int aheightY, int astepY){
+    originX = aoriginX; originY = aoriginY;
+    lengthX = alengthX; heightY = aheightY;
+    stepY = astepY; queue = q;
+}
+
+Graph::~Graph(){
+}
+
+void Graph::drawAxis(){
+    painter->setPen(Qt::black);
+    painter->drawLine(originX, originY, originX + lengthX, originY);
+    painter->drawLine(originX, originY, originX, originY - heightY);
+    painter->drawLine(originX + lengthX, originY, originX + lengthX - 15, originY - 5);
+    painter->drawLine(originX + lengthX, originY+1, originX + lengthX - 15, originY + 5);
+    painter->drawLine(originX, originY - heightY, originX - 5, originY - heightY + 15);
+    painter->drawLine(originX, originY - heightY, originX + 5, originY - heightY + 15);
+    /*
+        for(int i = stepY; i < heightY; i+=stepY){
+            painter->setPen(Qt::gray);
+            painter->drawLine(originX, originY - i, originX + lengthX, originY - i);
+            painter->setPen(Qt::black);
+        }*/
+}
+
+void Graph::draw(){
+    drawAxis();
+    for(auto &process: processes){
+        process.setPainter(painter);
+        process.draw();
+    }
+}
+
+void Graph::sendToQueue(Process proc){
+    queue->addProcess(proc);
+    processes.erase(processes.begin());
+}
+
+void Graph::update(int dx){
+    drawAxis();
+    for(auto &process: processes){
+        if(process.getX()<originX + dx){
+            sendToQueue(process);
+        }
+        else{
+            process.setPainter(painter);
+            process.move(dx,0);
+        }
+    }
+}
+
+void Graph::createProcess(QString name, int len, int height, QColor col){
+    bool free = false;
+    int y = originY - stepY;
+    while(y > originY - heightY && !free){
+        free = true;
+        for(auto process: processes){
+            if(process.getY() == y && process.getX() + process.getLength() >= originX + lengthX - 1)
+                free = false;
+        }
+        if(free){
+             processes.push_back(Process(name, originX + lengthX, y, len, height, col));
+             processes[processes.size()-1].setPainter(painter);
+             processes[processes.size()-1].draw();
+        }
+        y -= stepY;
+    }
+}
+
+
+
+//int Graph::getOriginX(){ return originX; }
+//int Graph::getOriginY(){ return originY; }
+//int Graph::getLengthX(){ return lengthX; }
+//int Graph::getHeightY(){ return heightY; }
+int Graph::getStepY(){ return stepY; }
+
+void Graph::setStepY(int step){ stepY = step; }
+//void Graph::setPainter(QPainter *paint){ painter = paint; }
+
+
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    update();
+    bar = new ExecutionBar;
+    queue = new Queue();
+    queue->setSlider(ui->queueSlider);
+    queue->setSorting(ui->algorithmBox);
+    graph = new Graph(queue);
+    //proc = new Process("П1", 1000, 120, 100);
+    //update();
     timer = new QTimer();
     connect(timer, SIGNAL(timeout()), this, SLOT(timerActivation()));
-    timer->start(50);
-    ui->axisXLabel->setGeometry(31, 75, 250, 100);
-
+    timer->start(10);
 }
 
 void MainWindow::paintEvent(QPaintEvent *event){
+    painter = new QPainter(this);
+    bar->setPainter(painter);
+    queue->setPainter(painter);
+
+    graph->setPainter(painter);
+
+   // proc->setPainter(painter);
     int xzero = 11;
-    int yzero = 55;
+    int yzero = 40;
     int width = 1208;
-    int height = 635;
-    QPainter painter(this);
-    painter.fillRect(xzero,yzero,width,height, QBrush(QColor(255,255,255)));
-    painter.setPen(Qt::black);
+    int height = 650;
+    painter->fillRect(xzero,yzero,width,height, QBrush(QColor(255,255,255)));
+    painter->setPen(Qt::black);
+    if(timeFlag){
+        // proc->move(-1,0);
 
-    painter.fillRect(xzero+20,yzero+20,100,25, QBrush(QColor(0,180,0,120)));
-    painter.drawRect(xzero+20,yzero+20,100,25);
-   // painter.drawRect(xzero+19,yzero+19,102,22);
+        queue->draw();
+        bar->drawLabel();
+         graph->update(-1);
+         if(rand()%200 + 198 < 200){
+             count++;
+            graph->createProcess("П" + QString::number(count),rand()%170 + 25, 25, QColor(rand()%156+100,rand()%156+100,rand()%156+100,120));
+         }
+    }
 
-    QWidget::paintEvent(event);
+    painter->fillRect(0,0,xzero,height+yzero, QBrush(QColor("#f0f0f0")));
+    painter->fillRect(xzero + width,0,xzero*5,height+yzero, QBrush(QColor("#f0f0f0")));
+        painter->fillRect(0,0,xzero*2 + width,yzero, QBrush(QColor("#f0f0f0")));
+    painter->end();
 }
 
 void MainWindow::timerActivation()
 {
-    /* Ежесекундно обновляем данные по текущему времени
-     * Перезапускать таймер не требуется
-     * */
     ui->labelSpeed->setText(QTime::currentTime().toString("hh:mm:ss"));
-    ui->axisXLabel->move(ui->axisXLabel->x()+1,ui->axisXLabel->y());
+    timeFlag = true;
+    this->repaint();
+    timeFlag = false;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete timer;
+    delete painter;
+    delete graph;
+    delete queue;
+    delete bar;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,144 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
 #include <QTimer>
+#include <QColor>
+#include <QSlider>
+#include <QComboBox>
+#include <QLabel>
+
+class Process{
+  public:
+    Process(QString aname = "", int ax = 0, int ay = 0,
+            int alength = 100, int aheight = 25, QColor acolor = QColor(0,180,0,120));
+    ~Process();
+    //void hide();
+    void draw(/*QColor col = Qt::black*/);
+    //void show(); //тоже самое, что и draw?
+    //void moveleft();
+    void move(int dx, int dy);
+
+    int getX();
+    int getY();
+    int getLength();
+    int getStartLength();
+    int getHeight();
+    QString getName();
+
+    void setX(int ax);
+    void setY(int ay);
+    void setLength(int len);
+    void setPainter(QPainter *paint);
+  private:
+    int x, y;
+    int length;
+    int startlength;
+    int height;
+    //int status;
+    QString name;
+    QColor color;
+    QPainter *painter;
+};
+
+class Area{
+public:
+  int getOriginX();
+  int getOriginY();
+  int getLengthX();
+  int getHeightY();
+
+  void setPainter(QPainter *paint);
+protected:
+  int originX, originY;
+  int lengthX, heightY;
+  std::vector <Process> processes;
+  QPainter *painter;
+};
+
+class ExecutionBar : public Area{
+public:
+    ExecutionBar(int aoriginX = 30, int aoriginY = 680,
+            int alengthX = 1170, int aheightY = 50);
+    ~ExecutionBar();
+    void drawLabel();
+    void draw();
+    void requestForNext();
+    void clear();
+};
+
+class Queue : public Area{
+public:
+  Queue(int aoriginX = 30, int aoriginY = 600,
+          int alengthX = 385, int aheightY = 520, int astepY = 40);
+  ~Queue();
+  void drawLabel();
+  void draw();
+  void sort();
+  void addProcess(Process proc);
+  Process findProcess();
+
+  int getStepY();
+
+  void setStepY(int step);
+  void setSlider(QSlider *sl);
+  void setSorting(QComboBox *sort);
+private:
+  int stepY;
+  QSlider *slider;
+  QComboBox *sorting;
+};
+
+class Graph : public Area{
+public:
+  Graph(Queue *q, int aoriginX = 450, int aoriginY = 600,
+          int alengthX = 740, int aheightY = 520, int astepY = 40);
+  ~Graph();
+  void drawAxis();
+  void draw();
+  void update(int dx);
+  void createProcess(QString name = "", int len = 100, int height = 25, QColor col = QColor(0,180,0,120));
+  void sendToQueue(Process proc);
+
+  int getStepY();
+
+  void setStepY(int step);
+private:
+  int stepY;
+  Queue *queue;
+};
+
+
+//class Graph{
+//public:
+//  Graph(int aoriginX = 100, int aoriginY = 600,
+//          int alengthX = 1000, int aheightY = 500, int astepY = 40);
+//  ~Graph();
+//  void drawAxis();
+//  void draw();
+//  void update(int dx);
+//  void createProcess(QString name = "", int len = 100, int height = 25, QColor col = QColor(0,180,0,120));
+//  void sendToQueue(Process proc);
+
+//  int getOriginX();
+//  int getOriginY();
+//  int getLengthX();
+//  int getHeightY();
+//  int getStepY();
+
+//  void setStepY(int step);
+//  void setPainter(QPainter *paint);
+//private:
+
+//  int originX, originY;
+//  int lengthX, heightY;
+//  int stepY;
+//  std::vector <Process> processes;
+//  QPainter *painter;
+//};
+
+
+
 
 namespace Ui {
 class MainWindow;
@@ -22,6 +159,13 @@ private slots:
 private:
     Ui::MainWindow *ui;
     QTimer *timer;
+    ExecutionBar * bar;
+    Queue *queue;
+    Graph *graph;
+    //Process *proc;
+    QPainter *painter;
+    bool timeFlag = false;
+    long long count = 0;
 };
 
 #endif // MAINWINDOW_H
